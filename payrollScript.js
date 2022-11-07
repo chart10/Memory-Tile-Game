3c90a4226f662b4c6103e519a29f1ffdf1e911f6
Christian Hart <christiantaylorhart@gmail.com>
2022-11-07 17:09:46 -0500

    added some stylings

--- payrollScript.js
+++ payrollScript.js
@@ -1,1 +1,6 @@
 // Christian Hart 001-68-3628
+
+function beginPayroll() {
+    window.alert("Welcome to the Payroll Department. Start by entering your" + 
+    "first employee's hours this week.");
+}
